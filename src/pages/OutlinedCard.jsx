581e6579a5c3e8b375fc4622e7ac26bfa92ee15d
narendraiiitl <LCS2019015@iiitl.ac.in>
2022-03-31 18:17:58 +0530

    updated

--- src/pages/OutlinedCard.jsx
+++ src/pages/OutlinedCard.jsx
@@ -11,50 +11,12 @@ import axios from "axios";
 import FacebookLogin from "react-facebook-login";
 
 const postonfb = (accessToken) => {
+  console.log(accessToken);
   const baseURL = `https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=${appid}&
 client_secret=${appsecret}&fb_exchange_token=${accessToken}`;
   axios.get(baseURL).then((response) => {
     console.log(response.data);
   });
-};
-
-const card = (data) => {
-  return (
-    <React.Fragment>
-      <Content data={data} />
-      <Box sx={{ width: "100%" }} className="wrapper">
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-        >
-          <Button variant="contained" color="info">
-            Message On LinkedIn
-          </Button>
-          <Button
-            variant="contained"
-            className="darkblue"
-            onClick={() => {
-              postonfb();
-            }}
-          >
-            Message On Facebook
-          </Button>
-          <Button variant="contained" color="info" startIcon={<TwitterIcon />}>
-            Tweet
-          </Button>
-          <Typography variant="body1" gutterBottom>
-            Enable automatic sharing to periodically share your referral link on
-            linkedIn
-          </Typography>
-          <Button variant="contained" color="info">
-            Auto share On LinkedIn
-          </Button>
-        </Stack>
-      </Box>
-    </React.Fragment>
-  );
 };
 
 export default function OutlinedCard(props) {
@@ -74,7 +36,45 @@ export default function OutlinedCard(props) {
   return (
     <Box sx={{ width: "70%", minWidth: 275 }}>
       <p>{props.obj}</p>
-      <Card variant="outlined">{card(props.data)}</Card>
+      <Card variant="outlined">
+        {" "}
+        <Content data={props.data} />
+        <Box sx={{ width: "100%" }} className="wrapper">
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Button variant="contained" color="info">
+              Message On LinkedIn
+            </Button>
+            <Button
+              variant="contained"
+              className="darkblue"
+              onClick={() => {
+                postonfb(accessToken);
+              }}
+            >
+              Message On Facebook
+            </Button>
+            <Button
+              variant="contained"
+              color="info"
+              startIcon={<TwitterIcon />}
+            >
+              Tweet
+            </Button>
+            <Typography variant="body1" gutterBottom>
+              Enable automatic sharing to periodically share your referral link
+              on linkedIn
+            </Typography>
+            <Button variant="contained" color="info">
+              Auto share On LinkedIn
+            </Button>
+          </Stack>
+        </Box>
+      </Card>
       {!login && (
         <FacebookLogin
           appId="691328388673523"
