1cda0179399cdfc2b17bfee1aa5e8a143e9ef6cf
narendraiiitl <LCS2019015@iiitl.ac.in>
2022-04-01 17:50:12 +0530

    updated'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import OutlinedCard from "./OutlinedCard";
-import data from "../dummydata";
+
+import axios from "axios";
 
 export default function Home() {
+  const [data, setData] = React.useState([]);
+  // const data = [];
+  useEffect(() => {
+    const baseURL = "https://refermeagain.herokuapp.com/";
+    axios.get(baseURL).then((response) => {
+      console.log(response);
+      setData(response.data);
+      // data = response.data;
+    });
+  }, []);
   return (
     <Box sx={{ width: "100%" }}>
       <Stack spacing={4} justifyContent="center" alignItems="center">
